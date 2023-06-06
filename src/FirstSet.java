d8b6896009fcc222e15a95491383aee7859ca7cb
jattinmanhas <63615545+jattinmanhas@users.noreply.github.com>
2023-06-06 22:40:28 +0530

    Solved more questions

--- src/FirstSet.java
+++ src/FirstSet.java
@@ -13,5 +13,55 @@ public class FirstSet {
     }
 
     // 2.Print Linearly from 1 T0 N
+    public void printLinearlyFrom1ToN(int n){
+        // base condition
+        if(n < 1){
+            return;
+        }
+        // calling recursive function until base condition returns false
+        printLinearlyFrom1ToN(n-1);
+
+        // printing values of n in the end...
+        System.out.println(n);
+    }
+    // 3. Print Numbers from N to 1
+    public void printFromNto1(int n){
+        // base condition
+        if(n < 1){
+            return;
+        }
+
+        // In this case printing values first as we are going to print values from n
+        System.out.println(n);
+
+        // calling function recursively...
+        printFromNto1(n-1);
+    }
+
+    // 4. Print Sum from 1 To N
+    public void printSumNSum(int num, int sum, int n){
+        // base condition
+        if(num > n){
+            // printing sum
+            System.out.println(sum);
+            return;
+        }
+
+        // sum
+        sum += num;
+        // calling recursive funtion...
+        printSumNSum(num + 1, sum , n);
+    }
+
+    // 5. print values from 1 to n by returning value
+    public int printReturnSum(int num){
+        // base condition
+        if(num < 1){
+            return 0;
+        }
+
+        // adding values of n to the recursive function.. => 5 + fn -> 4 + fn -> 3 + fn
+        return num +  printReturnSum(num -1);
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         FirstSet fs = new FirstSet();
-        fs.printName5Times("Jattin", 5);
+//        fs.printName5Times("Jattin", 5);
+//        fs.printLinearlyFrom1ToN(8);
+//        fs.printFromNto1(6);
+        fs.printSumNSum(1, 0, 5);
+        System.out.println(fs.printReturnSum( 5));
     }
 }
