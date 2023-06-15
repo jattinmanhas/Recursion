38dae2cc3372a82b137220592e88fa6e7263776e
jattinmanhas <63615545+jattinmanhas@users.noreply.github.com>
2023-06-15 23:21:12 +0530

    Solved count n-queens

--- src/AdvancedRecursion.java
+++ src/AdvancedRecursion.java
@@ -120,5 +120,63 @@ public class AdvancedRecursion {
         }
        return list;
     }
+
+    // 4. N-queens number of ways to insert queens
+    public int totalNQueens(int n) {
+        boolean[][] board = new boolean[n][n];
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j<n;j++){
+                board[i][j] = false;
+            }
+        }
+
+        int ans = nQueensPaths(0, board);
+        return ans;
+    }
+
+    public int nQueensPaths(int row, boolean[][] board){
+        if(row == board.length){
+            return 1;
+        }
+
+        int count = 0;
+
+        for(int col=0; col<board.length;col++){
+            if(isSafe(board, row, col)){
+                board[row][col] = true;
+                count += nQueensPaths( row+1,board);
+                board[row][col] = false;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean isSafe(boolean[][] board, int row, int col){
+        //check vertical
+        for(int i=0;i<row;i++){
+            if(board[i][col]){
+                return false;
+            }
+        }
+
+        //check diagonal left
+        int maxLeft = Math.min(row, col);
+        for(int i=1;i<=maxLeft;i++){
+            if(board[row-i][col-i]){
+                return false;
+            }
+        }
+
+        //check diagonal right
+        int maxRight = Math.min(row, board.length-col-1);
+        for(int i=1;i<=maxRight;i++)
+        {
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
